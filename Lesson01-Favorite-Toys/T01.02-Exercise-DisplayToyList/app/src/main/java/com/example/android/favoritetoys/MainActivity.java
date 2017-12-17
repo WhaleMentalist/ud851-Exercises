36568af2efd8c05a6e9b98f26a74adf78109f8f7
Spencer Daniel <spencerdaniel93@gmail.com>
2017-12-16 19:52:56 -0600

    Completed lesson 3.

--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         // COMPLETED (5) Loop through each toy and append the name to the TextView (add \n for spacing)
         for(String toyName : toys) {
-            mToyListTextView.append(toyName + "\n");
+            mToyListTextView.append(toyName + "\n\n\n");
         }
     }
 }
